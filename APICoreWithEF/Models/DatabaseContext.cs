1cd4fe30ed69d7c1616fceeb841a742c7130bd8d
HIENLTH <hyhien@gmail.com>
2018-03-02 14:24:18 +0700

    Add project files.

---
+++ APICoreWithEF/Models/DatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICoreWithEF.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+
+
+        //add DB
+        public DbSet<CustomerTB> CustomerTB { get; set; }
+    }
+}
